f31e3fc0d3f43c142cacc1e81a069efe6ea44e5f
Nick Charlton <hello@nickcharlton.net>
2013-12-09 16:00:46 +0000

    Installs collectd and the collectd plugin.

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,14 @@
 # 
 # MIT
 #
+
+# we define the settings slightly differently
+node.normal[:collectd_librato][:email] = node[:monitoring][:librato][:email]
+node.normal[:collectd_librato][:api_token] = node[:monitoring][:librato][:api_token]
+
+# install collectd and the librato plugin
+include_recipe 'collectd'
+include_recipe 'collectd-librato'
+
+# build up the monitoring plugins
+
